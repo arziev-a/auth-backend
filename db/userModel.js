854eabd4f6c70aeafa36b46863436e3ca68b5b55
arziev-a <alinurarziev@gmail.com>
2022-10-19 18:13:07 +0200

    Implemented backend part of the CRUD App

---
+++ db/userModel.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, 'Please provide an email!'],
+    unique: [true, 'Email exists!'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password!'],
+    unique: false,
+  },
+})
+
+module.exports = mongoose.model.Users || mongoose.model("Users", userSchema);
